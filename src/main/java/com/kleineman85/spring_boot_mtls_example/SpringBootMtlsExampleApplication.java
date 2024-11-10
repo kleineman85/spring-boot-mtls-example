78233f3933a7b81fd446c11b2b4757d4519827c4
Joël Sanwiradi <joel@sanwiradi.com>
2024-11-10 13:26:04 +0100

    initial commit

---
+++ src/main/java/com/kleineman85/spring_boot_mtls_example/SpringBootMtlsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kleineman85.spring_boot_mtls_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMtlsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMtlsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleineman85/spring_boot_mtls_example/SpringBootMtlsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleineman85.spring_boot_mtls_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMtlsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
